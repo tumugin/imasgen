a5a434d5950ee2b2ab742334561c2a3e4eb6b1fb
Kazuki Oishi <oishikazuki@gmail.com>
2018-03-12 21:43:07 +0900

    Generate name more faster.

--- lib/imasgen/generator_base.rb
+++ lib/imasgen/generator_base.rb
@@ -2,7 +2,8 @@ require 'imasgen/name'
 
 module Imasgen
   class GeneratorBase
-    attr_accessor :auto_reset, :be_strict
+    attr_accessor :auto_reset
+    attr_reader :be_strict
 
     def initialize
       @namedic = []
@@ -11,42 +12,56 @@ module Imasgen
       @be_strict = true
     end
 
+    def reset
+      # reset dictonary cache
+      @basedic = nil
+      @first_names = nil
+      @last_names = nil
+      @comb = nil
+    end
+
+    def be_strict=(val)
+      @be_stricte = val
+      reset
+    end
+
     def name
       # return random item.(no shuffle in first name and last name)
       item = lambda {
-        basedic = @be_strict ? @namedic.select {|i| i[:last_name] != '' && i[:first_name] != ''} : @namedic
-        retval = (basedic - @used_pair).sample
+        @basedic ||= @be_strict ? @namedic.select {|i| i[:last_name] != '' && i[:first_name] != ''} : @namedic
+        retval = @basedic.reject {|i| i[:used]}.sample
         throw StandardError.new('Already used all name pairs.') if retval.nil? && !@auto_reset
         if retval.nil? && @auto_reset
-          @used_pair.clear
+          # delete used flag
+          @basedic.each {|i| i.delete(:used)}
           retval = @namedic.sample
         end
         retval
       }.call
-      @used_pair << item
+      item[:used] = true
       Imasgen::Name.new(item)
     end
 
     def hotchpotch
       # hotchpotch mode
       item = lambda {
-        basedic = @be_strict ? @namedic.select {|i| i[:last_name] != '' && i[:first_name] != ''} : @namedic
+        @basedic ||= @be_strict ? @namedic.select {|i| i[:last_name] != '' && i[:first_name] != ''} : @namedic
         # make shuffle dictionary
-        first_names = basedic.map {|i| {first_name: i[:first_name], first_name_furigana: i[:first_name_furigana]}}
-        last_names = basedic.map {|i| {last_name: i[:last_name], last_name_furigana: i[:last_name_furigana]}}
+        @first_names ||= @basedic.map {|i| {first_name: i[:first_name], first_name_furigana: i[:first_name_furigana]}}
+        @last_names ||= @basedic.map {|i| {last_name: i[:last_name], last_name_furigana: i[:last_name_furigana]}}
         # shuffle!!
-        comb = first_names.product(last_names).map {|i| i[0].merge(i[1])}
+        @comb ||= @first_names.product(@last_names).map {|i| i[0].merge(i[1])}
         # get non duplicate item
-        ret = (comb - @used_pair).sample
+        ret = @comb.reject {|i| i[:used]}.sample
         return ret unless ret.nil?
         if @auto_reset
-          @used_pair.clear
+          @comb.each {|i| i.delete(:used)}
           ret
         else
           throw StandardError.new('Already used all name pairs.')
         end
       }.call
-      @used_pair << item
+      item[:used] = true
       Imasgen::Name.new(item)
     end
   end
