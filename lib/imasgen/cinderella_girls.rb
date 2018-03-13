8e63f0b1932dfdfc5d873898ed7ee3d68f41ec7f
Kazuki Oishi <oishikazuki@gmail.com>
2018-03-14 01:37:36 +0900

    Mirai-chan< Dehehe~~

--- lib/imasgen/cinderella_girls.rb
+++ lib/imasgen/cinderella_girls.rb
@@ -8,10 +8,10 @@ module Imasgen
       yaml = YAML.load_file(__dir__ + '/dic/cinderella_girls.yml')
       @namedic = yaml[:cinderella_girls]
     end
+  end
 
-    def self.cinderella
-      @imas_cg ||= Imasgen::CinderellaGirlsCls.new
-      @imas_cg
-    end
+  def self.cinderella
+    @imas_cg ||= Imasgen::CinderellaGirlsCls.new
+    @imas_cg
   end
 end
